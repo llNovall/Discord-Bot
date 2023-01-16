06a1e315f554efafec6103510c0b3e9259c75ee4
llNovall <25434267+llNovall@users.noreply.github.com>
2023-01-16 21:10:15 +0100

    Removed Roles and Channel Finder. Delegated the task to GuildManager.

--- Discord Bot/Discord Bot/Database/DatabaseManager.cs
+++ Discord Bot/Discord Bot/Database/DatabaseManager.cs
@@ -1,17 +1,24 @@
-﻿using Microsoft.Data.SqlClient;
+﻿using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.Entities;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace Discord_Bot.Database
 {
     public class DatabaseManager
     {
+        private ILogger<BaseDiscordClient> _logger;
         private string _connectionString { get; }
 
-        public DatabaseManager()
+        public DatabaseManager(ILogger<BaseDiscordClient> logger)
         {
+            _logger = logger;
             _connectionString = ConfigurationManager.ConnectionStrings["default_connection"].ToString();
         }
 
@@ -50,7 +57,7 @@ namespace Discord_Bot.Database
             }
             catch (Exception e)
             {
-                Console.WriteLine(e.Message);
+                _logger.Log(LogLevel.Error, message: $"Failed to retreive values for channel usage {channel_usage_type} for guild {guildId}.\n Error : {e.Message}");
             }
 
             return data;
@@ -81,18 +88,176 @@ namespace Discord_Bot.Database
             }
             catch (Exception e)
             {
-                Console.WriteLine(e.Message);
+                _logger.Log(LogLevel.Error, message: $"Failed to update values for channel usage for channel {channelId} for guild {guildId}.\n Error : {e.Message}");
             }
 
             return false;
         }
 
-        public async Task<Dictionary<string, ulong>> GetRoleForGuildRules(ulong guildId)
+        public async Task<Dictionary<string, ulong>> GetChannelUsageDataForGuild(ulong guildId)
         {
-            string query = "SELECT [guild_id], [role_id] FROM [DbDiscord].[dbo].[TbRules] WHERE [guild_id] = @GuildId";
+            string query = "SELECT [channel_usage_type], [channel_id] FROM [DbDiscord].[dbo].[TbGuildChannelUsage] WHERE [guild_id] = @GuildId";
 
-            Dictionary<string, ulong> data = new Dictionary<string, ulong>();
+            Dictionary<string, ulong> data = new();
 
+            try
+            {
+                using SqlConnection myConnection = new SqlConnection(_connectionString);
+                using (SqlCommand oCmd = new SqlCommand(query, myConnection))
+                {
+                    oCmd.Parameters.Add("@GuildId", System.Data.SqlDbType.BigInt).Value = guildId;
+
+                    await myConnection.OpenAsync();
+
+                    using SqlDataReader oReader = await oCmd.ExecuteReaderAsync();
+                    while (await oReader.ReadAsync())
+                    {
+                        data.Add(oReader["channel_usage_type"].ToString(), Convert.ToUInt64(oReader["channel_id"]));
+                    }
+
+                    myConnection.Close();
+                }
+
+                await myConnection.CloseAsync();
+            }
+            catch (Exception e)
+            {
+                _logger.Log(LogLevel.Error, message: $"Failed to retreive values for guild {guildId}.\n Error : {e.Message}");
+            }
+
+            return data;
+        }
+
+        public async Task<ulong> GetRoleForGuild(ulong guildId, string roleUsage)
+        {
+            string query = "SELECT [role_id] FROM [DbDiscord].[dbo].[TbGuildRoles] WHERE [guild_id] = @GuildId AND [role_usage] = @RoleUsage";
+
+            ulong roleId = 0;
+
+            try
+            {
+                using (SqlConnection myConnection = new SqlConnection(_connectionString))
+                {
+                    using (SqlCommand oCmd = new SqlCommand(query, myConnection))
+                    {
+                        oCmd.Parameters.Add("@GuildId", System.Data.SqlDbType.BigInt).Value = guildId;
+                        oCmd.Parameters.Add("@RoleUsage", System.Data.SqlDbType.VarChar).Value = roleUsage;
+
+                        await myConnection.OpenAsync();
+
+                        roleId = Convert.ToUInt64(await oCmd.ExecuteScalarAsync());
+                    }
+
+                    await myConnection.CloseAsync();
+                }
+            }
+            catch (Exception e)
+            {
+                _logger.Log(LogLevel.Error, message: $"Failed to retreive values for role for {roleUsage} in guild {guildId}.\n Error : {e.Message}");
+            }
+
+            return roleId;
+        }
+
+        public async Task<Dictionary<string, ulong>> GetRolesForGuild(ulong guildId)
+        {
+            string query = "SELECT [role_id], [role_usage] FROM [DbDiscord].[dbo].[TbGuildRoles] WHERE [guild_id] = @GuildId";
+
+            Dictionary<string, ulong> rolesDict = new Dictionary<string, ulong>();
+
+            try
+            {
+                using (SqlConnection myConnection = new SqlConnection(_connectionString))
+                {
+                    using (SqlCommand oCmd = new SqlCommand(query, myConnection))
+                    {
+                        oCmd.Parameters.Add("@GuildId", System.Data.SqlDbType.BigInt).Value = guildId;
+
+                        await myConnection.OpenAsync();
+
+                        using SqlDataReader dataReader = await oCmd.ExecuteReaderAsync();
+                        while (await dataReader.ReadAsync())
+                        {
+                            rolesDict.Add(dataReader["role_usage"].ToString(),
+                                        Convert.ToUInt64(dataReader["role_id"]));
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                _logger.Log(LogLevel.Error, message: $"Failed to retrieve roles for guild {guildId}.\n Error : {e.Message}");
+            }
+
+            return rolesDict;
+        }
+
+        public async Task<bool> UpdateRoleForGuild(ulong guildId, ulong roleId, string roleUsage)
+        {
+            try
+            {
+                string query = "IF NOT EXISTS (SELECT * FROM DbDiscord.dbo.TbGuild WHERE guild_id = @GuildId) INSERT INTO DbDiscord.dbo.TbGuild (guild_id) VALUES (@GuildId) UPDATE DbDiscord.dbo.TbGuildRoles set role_id = @RoleId WHERE guild_id = @GuildId AND role_usage = @RoleUsage IF @@ROWCOUNT=0 INSERT INTO DbDiscord.dbo.TbGuildRoles (guild_id, role_id, role_usage) values(@GuildId, @RoleId, @RoleUsage)";
+
+                using (SqlConnection myConnection = new SqlConnection(_connectionString))
+                {
+                    using (SqlCommand oCmd = new SqlCommand(query, myConnection))
+                    {
+                        oCmd.Parameters.Add("@GuildId", System.Data.SqlDbType.BigInt).Value = guildId;
+                        oCmd.Parameters.Add("@RoleId", System.Data.SqlDbType.BigInt).Value = roleId;
+                        oCmd.Parameters.Add("@RoleUsage", System.Data.SqlDbType.VarChar).Value = roleUsage;
+
+                        await myConnection.OpenAsync();
+
+                        await oCmd.ExecuteNonQueryAsync();
+                    }
+
+                    await myConnection.CloseAsync();
+                }
+                return true;
+            }
+            catch (Exception e)
+            {
+                _logger.Log(LogLevel.Error, message: $"Failed to update values for roles for guild rules for guild {guildId}.\n Error : {e.Message}");
+            }
+
+            return false;
+        }
+
+        public async Task<bool> UpdateLoggerStatusForGuild(ulong guildId, bool isEnabled)
+        {
+            try
+            {
+                string query = "IF NOT EXISTS (SELECT * FROM DbDiscord.dbo.TbGuild WHERE guild_id = @GuildId) INSERT INTO DbDiscord.dbo.TbGuild (guild_id) VALUES (@GuildId) UPDATE DbDiscord.dbo.TbLogger set is_enabled = @IsEnabled WHERE guild_id = @GuildId IF @@ROWCOUNT=0 INSERT INTO DbDiscord.dbo.TbLogger (guild_id, is_enabled) values(@GuildId, @IsEnabled)";
+
+                using (SqlConnection myConnection = new SqlConnection(_connectionString))
+                {
+                    using (SqlCommand oCmd = new SqlCommand(query, myConnection))
+                    {
+                        oCmd.Parameters.Add("@GuildId", System.Data.SqlDbType.BigInt).Value = guildId;
+                        oCmd.Parameters.Add("@IsEnabled", System.Data.SqlDbType.Bit).Value = isEnabled ? 1 : 0;
+
+                        await myConnection.OpenAsync();
+
+                        await oCmd.ExecuteNonQueryAsync();
+                    }
+
+                    await myConnection.CloseAsync();
+                }
+                return true;
+            }
+            catch (Exception e)
+            {
+                _logger.Log(LogLevel.Error, message: $"Failed to update values for logger status for guild {guildId}.\n Error : {e.Message}");
+            }
+
+            return false;
+        }
+
+        public async Task<bool> GetLoggerStatusForGuild(ulong guildId)
+        {
+            string query = "SELECT [is_enabled]FROM [DbDiscord].[dbo].[TbLogger] WHERE [guild_id] = @GuildId";
+
+            bool isLoggerEnabled = false;
             try
             {
                 using (SqlConnection myConnection = new SqlConnection(_connectionString))
@@ -107,8 +272,7 @@ namespace Discord_Bot.Database
                         {
                             while (await oReader.ReadAsync())
                             {
-                                data.Add("guild_id", Convert.ToUInt64(oReader["guild_id"]));
-                                data.Add("role_id", Convert.ToUInt64(oReader["role_id"]));
+                                isLoggerEnabled = (bool)oReader["is_enabled"];
                             }
 
                             myConnection.Close();
@@ -120,40 +284,10 @@ namespace Discord_Bot.Database
             }
             catch (Exception e)
             {
-                Console.WriteLine(e.Message);
+                _logger.Log(LogLevel.Error, message: $"Failed to retreive values for logger status for guild {guildId}.\n Error : {e.Message}");
             }
 
-            return data;
-        }
-
-        public async Task<bool> UpdateRoleForGuildRules(ulong guildId, ulong roleId)
-        {
-            try
-            {
-                string query = "IF NOT EXISTS (SELECT * FROM DbDiscord.dbo.TbGuild WHERE guild_id = @GuildId) INSERT INTO DbDiscord.dbo.TbGuild (guild_id) VALUES (@GuildId) UPDATE DbDiscord.dbo.TbRules set role_id = @RoleId WHERE guild_id = @GuildId IF @@ROWCOUNT=0 INSERT INTO DbDiscord.dbo.TbRules (guild_id, role_id) values(@GuildId, @RoleId)";
-
-                using (SqlConnection myConnection = new SqlConnection(_connectionString))
-                {
-                    using (SqlCommand oCmd = new SqlCommand(query, myConnection))
-                    {
-                        oCmd.Parameters.Add("@GuildId", System.Data.SqlDbType.BigInt).Value = guildId;
-                        oCmd.Parameters.Add("@RoleId", System.Data.SqlDbType.BigInt).Value = roleId;
-
-                        await myConnection.OpenAsync();
-
-                        await oCmd.ExecuteNonQueryAsync();
-                    }
-
-                    await myConnection.CloseAsync();
-                }
-                return true;
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.Message);
-            }
-
-            return false;
+            return isLoggerEnabled;
         }
     }
 }
---
+++ Discord Bot/Discord Bot/Services/GuildManager.cs
@@ -0,0 +1,226 @@
+﻿using Discord_Bot.Database;
+using Discord_Bot.Services.DataClasses;
+using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.Entities;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Discord_Bot.Services
+{
+    public class GuildManager
+    {
+        private DatabaseManager _databaseManager;
+        private ILogger<BaseDiscordClient> _logger;
+        private Dictionary<ulong, GuildData> _guildData = new();
+        private EventId _eventId = new EventId(701, "Guild Manager");
+
+        public bool IsReady;
+
+        public GuildManager(DatabaseManager databaseManager, ILogger<BaseDiscordClient> logger)
+        {
+            _databaseManager = databaseManager;
+            _logger = logger;
+        }
+
+        #region Channel
+
+        public DiscordChannel GetChannelFor(string channel_usage_type, CommandContext ctx)
+        {
+            if (string.IsNullOrEmpty(channel_usage_type))
+            {
+                _logger.LogError(_eventId, "Channel Usage Type is null or empty.");
+                return null;
+            }
+
+            if (!_guildData.ContainsKey(ctx.Guild.Id))
+            {
+                _logger.LogError(_eventId, $"Failed to find GuildData for guild {ctx.Guild.Name}");
+                return null;
+            }
+
+            GuildData data = _guildData[ctx.Guild.Id];
+
+            return !data.ChannelsDict.ContainsKey(channel_usage_type) ? null : ctx.Guild.GetChannel(data.ChannelsDict[channel_usage_type]);
+        }
+
+        public DiscordChannel GetChannelFor(string channel_usage_type, DiscordGuild discordGuild)
+        {
+            if (string.IsNullOrEmpty(channel_usage_type))
+            {
+                _logger.LogError(_eventId, "Channel Usage Type is null or empty.");
+                return null;
+            }
+
+            if (!_guildData.ContainsKey(discordGuild.Id))
+            {
+                _logger.LogError(_eventId, $"Failed to find GuildData for guild {discordGuild.Name}");
+                return null;
+            }
+
+            GuildData data = _guildData[discordGuild.Id];
+            return !data.ChannelsDict.ContainsKey(channel_usage_type) ? null : discordGuild.GetChannel(data.ChannelsDict[channel_usage_type]);
+        }
+
+        public async Task<bool> UpdateChannelUsageForChannel(DiscordGuild discordGuild, DiscordChannel discordChannel, string channelUsage)
+        {
+            if (string.IsNullOrEmpty(channelUsage))
+            {
+                _logger.LogError(_eventId, "Channel Usage Type is null or empty.");
+                return false;
+            }
+
+            if (!_guildData.ContainsKey(discordGuild.Id))
+            {
+                _logger.LogError(_eventId, $"Failed to find GuildData for guild {discordGuild.Name}");
+                return false;
+            }
+
+            GuildData guildDatadata = _guildData[discordGuild.Id];
+
+            if (guildDatadata.ChannelsDict.ContainsKey(channelUsage))
+                guildDatadata.ChannelsDict[channelUsage] = discordChannel.Id;
+            else
+                guildDatadata.ChannelsDict.Add(channelUsage, discordChannel.Id);
+
+            return await _databaseManager.UpdateChannelUsageForChannel(discordGuild.Id, discordChannel.Id, channelUsage);
+        }
+
+        #endregion Channel
+
+        #region Logger
+
+        public bool GetLoggerStatusForGuild(DiscordGuild discordGuild)
+        {
+            if (!_guildData.ContainsKey(discordGuild.Id))
+            {
+                _logger.LogError(_eventId, $"Failed to find GuildData for guild {discordGuild.Name}");
+                return false;
+            }
+
+            GuildData guildData = _guildData[discordGuild.Id];
+
+            return guildData.IsLoggerEnabled;
+        }
+
+        public async Task<bool> UpdateLoggerStatusForGuild(DiscordGuild discordGuild, bool isEnabled)
+        {
+            if (!_guildData.ContainsKey(discordGuild.Id))
+            {
+                _logger.LogError(_eventId, $"Failed to find GuildData for guild {discordGuild.Name}");
+                return false;
+            }
+
+            GuildData guildData = _guildData[discordGuild.Id];
+
+            guildData.IsLoggerEnabled = isEnabled;
+
+            return await _databaseManager.UpdateLoggerStatusForGuild(discordGuild.Id, isEnabled);
+        }
+
+        #endregion Logger
+
+        #region Role
+
+        public DiscordRole GetRoleFor(DiscordGuild discordGuild, string roleUsage)
+        {
+            if (string.IsNullOrEmpty(roleUsage))
+            {
+                _logger.LogError(_eventId, $"RoleUsage is null or empty.");
+                return null;
+            }
+
+            if (!_guildData.ContainsKey(discordGuild.Id))
+            {
+                _logger.LogError(_eventId, $"Failed to find GuildData for guild {discordGuild.Name}");
+                return null;
+            }
+
+            GuildData guildData = _guildData[discordGuild.Id];
+
+            if (guildData.RolesDict.ContainsKey(roleUsage))
+                return discordGuild.GetRole(guildData.RolesDict[roleUsage]);
+
+            return null;
+        }
+
+        public Dictionary<string, DiscordRole> GetRolesFor(DiscordGuild discordGuild)
+        {
+            if (!_guildData.ContainsKey(discordGuild.Id))
+            {
+                _logger.LogError(_eventId, $"Failed to find GuildData for guild {discordGuild.Name}");
+                return null;
+            }
+
+            GuildData guildData = _guildData[discordGuild.Id];
+            Dictionary<string, DiscordRole> rolesDict = new Dictionary<string, DiscordRole>();
+
+            foreach (var role in guildData.RolesDict)
+            {
+                DiscordRole discordRole = discordGuild.GetRole(role.Value);
+
+                if (discordRole != null)
+                {
+                    rolesDict.Add(role.Key, discordRole);
+                }
+            }
+
+            return rolesDict;
+        }
+
+        public async Task<bool> UpdateRoleFor(DiscordGuild discordGuild, DiscordRole discordRole, string roleUsage)
+        {
+            if (string.IsNullOrEmpty(roleUsage))
+            {
+                _logger.LogError(_eventId, $"RoleUsage is null or empty.");
+                return false;
+            }
+
+            if (!_guildData.ContainsKey(discordGuild.Id))
+            {
+                _logger.LogError(_eventId, $"Failed to find GuildData for guild {discordGuild.Name}");
+                return false;
+            }
+
+            GuildData guildData = _guildData[discordGuild.Id];
+
+            if (guildData.RolesDict.ContainsKey(roleUsage))
+                guildData.RolesDict[roleUsage] = discordRole.Id;
+            else
+                guildData.RolesDict.Add(roleUsage, discordRole.Id);
+
+            return await _databaseManager.UpdateRoleForGuild(discordGuild.Id, discordRole.Id, roleUsage);
+        }
+
+        #endregion Role
+
+        public async Task LoadGuildDataFromDatabase(IEnumerable<DiscordGuild> discordGuilds)
+        {
+            foreach (var item in discordGuilds)
+            {
+                if (!_guildData.ContainsKey(item.Id))
+                    _guildData.Add(item.Id, await GetGuildDataForGuildFromDatabase(item));
+            }
+
+            IsReady = true;
+        }
+
+        public async Task<GuildData> GetGuildDataForGuildFromDatabase(DiscordGuild discordGuild)
+        {
+            bool isLoggerEnabled = await _databaseManager.GetLoggerStatusForGuild(discordGuild.Id);
+
+            GuildData guildData = new()
+            {
+                IsLoggerEnabled = isLoggerEnabled,
+                ChannelsDict = await _databaseManager.GetChannelUsageDataForGuild(discordGuild.Id),
+                RolesDict = await _databaseManager.GetRolesForGuild(discordGuild.Id)
+            };
+
+            return guildData;
+        }
+    }
+}
