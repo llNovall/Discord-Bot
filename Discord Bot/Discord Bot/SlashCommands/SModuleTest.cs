863c84e26684ebc7f5e44b8803426df5b2fded68
llNovall <25434267+llNovall@users.noreply.github.com>
2023-01-21 06:40:10 +0100

    Added a test slash module

---
+++ Discord Bot/Discord Bot/SlashCommands/SModuleTest.cs
@@ -0,0 +1,51 @@
+﻿using DSharpPlus.Entities;
+using DSharpPlus;
+using DSharpPlus.SlashCommands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Discord_Bot.SlashCommands
+{
+    public class SModuleTest : ApplicationCommandModule
+    {
+        [SlashCommand("test", "A slash command made to test the DSharpPlus Slash Commands extension!")]
+        public async Task TestCommand(InteractionContext ctx)
+        {
+            await ctx.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, new DiscordInteractionResponseBuilder().WithContent("Success!"));
+        }
+
+        [SlashCommand("delaytest", "A slash command made to test the DSharpPlus Slash Commands extension!")]
+        public async Task DelayTestCommand(InteractionContext ctx)
+        {
+            await ctx.CreateResponseAsync(InteractionResponseType.DeferredChannelMessageWithSource);
+
+            //Some time consuming task like a database call or a complex operation
+
+            await ctx.EditResponseAsync(new DiscordWebhookBuilder().WithContent("Thanks for waiting!"));
+        }
+
+        public class TestChoiceProvider : IChoiceProvider
+        {
+            public async Task<IEnumerable<DiscordApplicationCommandOptionChoice>> Provider()
+            {
+                return new DiscordApplicationCommandOptionChoice[]
+                {
+                    //You would normally use a database call here
+                    new DiscordApplicationCommandOptionChoice("testing", "testing"),
+                    new DiscordApplicationCommandOptionChoice("testing2", "test option 2")
+                };
+            }
+        }
+
+        [SlashCommand("choiceprovider", "test")]
+        public async Task ChoiceProviderCommand(InteractionContext ctx,
+            [ChoiceProvider(typeof(TestChoiceProvider))]
+            [Option("option", "option")] string option)
+        {
+            await ctx.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, new DiscordInteractionResponseBuilder().WithContent(option));
+        }
+    }
+}
